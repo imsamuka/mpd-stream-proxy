2ceb02d12daf66aa8456dc7d90923c61b7896808
imsamuka <imsamuka@gmail.com>
2023-02-05 09:45:48 -0300

    add ask_stream_infos()

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use anyhow::{anyhow, Error, Result};
 use futures::TryFutureExt;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server, StatusCode, Uri};
-use log::{debug, error, info as _, warn as _};
+use log::{debug, error, info, trace, warn};
+use serde_json::Value;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use tokio::process::Command;
-//use std::convert::Infallible;
 //use {std::time::Duration, tokio::time::sleep};
 
 const YTDL: &str = "yt-dlp";
@@ -18,7 +18,7 @@ type Client = hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnect
 
 #[tokio::main]
 async fn main() {
-    simple_logger::init_with_level(log::Level::Debug).unwrap();
+    simple_logger::init_with_env().unwrap();
 
     let client = hyper::Client::builder().build::<_, Body>(hyper_tls::HttpsConnector::new());
 
@@ -53,21 +53,17 @@ async fn main() {
 
 async fn handle_request(mut request: Request<Body>, client: Client) -> Result<Response<Body>> {
     let (input, is_asking_cover) = extract_input(request.uri().path_and_query().unwrap().as_str())?;
-    debug!("input: {input}");
+    info!("received input: {input}");
 
     if is_asking_cover {
-        error!("Asked for cover.");
-        return Ok::<_, Error>(
-            Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::empty())?,
-        );
+        Err(anyhow!("Asked for cover."))?
     }
 
-    let stream_url = ask_stream_url(input).await?;
+    let input_info = ask_stream_infos(&input).await?;
+    let stream_url = stream_url_from_info(&input_info[0])?;
     debug!("stream_url: {:?}", stream_url.get(0..65));
 
-    *request.uri_mut() = Uri::from_str(&stream_url)?;
+    *request.uri_mut() = Uri::from_str(stream_url)?;
     request.headers_mut().remove("host");
     debug!("request: {request:#?}");
     let response = client.request(request).await?;
@@ -89,22 +85,59 @@ fn extract_input(path_and_query: &str) -> Result<(String, bool)> {
     Ok((input.to_string(), is_asking_cover))
 }
 
-async fn ask_stream_url(input: String) -> Result<String> {
+fn stream_url_from_info(info: &Value) -> Result<&str> {
+    info.get("url")
+        .ok_or(anyhow!("no \"url\" present in info"))?
+        .as_str()
+        .ok_or(anyhow!("\"url\" is not a string"))
+}
+
+async fn ask_stream_infos(input: &str) -> Result<Vec<Value>> {
     let child = Command::new(YTDL)
-        .args(["-f", "bestaudio", "-g", &input])
+        .args(["-f", "bestaudio", "-j", input])
         .stdout(std::process::Stdio::piped())
         .spawn()?;
 
     let output = child.wait_with_output().await?;
 
-    Ok(match output.status.success() {
+    match output.status.success() {
+        true => {
+            let mut infos = Vec::<Value>::with_capacity(20); // bigger than most albums
+
+            for raw_json in std::str::from_utf8(&output.stdout)?.lines() {
+                match serde_json::from_str::<Value>(raw_json) {
+                    Ok(info) => infos.push(info),
+                    Err(e) => warn!("couldn't parse JSON: {e}"),
+                }
+            }
+
+            if infos.is_empty() {
+                Err(anyhow!("received no info from {YTDL}."))?
+            }
+
+            Ok(infos)
+        }
+        false => Err(anyhow!("child process failed to gather info."))?,
+    }
+}
+
+#[allow(unused)]
+async fn ask_stream_url(input: &str) -> Result<String> {
+    let child = Command::new(YTDL)
+        .args(["-f", "bestaudio", "-g", input])
+        .stdout(std::process::Stdio::piped())
+        .spawn()?;
+
+    let output = child.wait_with_output().await?;
+
+    match output.status.success() {
         true => {
             let stream_url = std::str::from_utf8(&output.stdout)?.trim_end().to_string();
             if stream_url.is_empty() {
                 Err(anyhow!("received empty stream_url from {YTDL}."))?
             }
-            stream_url
+            Ok(stream_url)
         }
         false => Err(anyhow!("child process failed."))?,
-    })
+    }
 }
